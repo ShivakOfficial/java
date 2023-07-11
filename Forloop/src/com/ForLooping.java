9eaca75c3f6a54988f7e2402ede1e2381ae6c5d1
ShivakOfficial <shivuyk57@gmail.com>
2023-07-11 19:10:52 +0530

    Basic Programms
    simple Programms to start learning

---
+++ Forloop/src/com/ForLooping.java
@@ -0,0 +1,14 @@
+package com;
+
+public class ForLooping {
+public static void main(String[] args) {
+
+for(int i=1;i<=5;i++)
+
+{
+
+System.out.println("Kodnest "+i);
+}
+}
+}
+
---
+++ Whileloop/src/com/WhileLoop.java
@@ -0,0 +1,13 @@
+package com;
+
+public class WhileLoop 
+{
+public static void main(String[] args) {
+	int i=1;
+	while(i<=5)
+	{
+		System.out.println("Kodnest");
+		i++;
+	}
+}
+}
